4056ff0b1af1db40ba0a16f86ad56105978eec1b
Peter <monroepe@gmail.com>
2014-08-27 15:22:16 -0400

    Connected database to articles index and submit new article page

--- schema.sql
+++ schema.sql
@@ -1,13 +1,13 @@
 CREATE TABLE articles (
   id serial PRIMARY KEY,
   title varchar(255) NOT NULL,
-  url varchar(255) NOT NULL,
-  description varchar(10000) NOT NULL
+  url varchar(2038) NOT NULL,
+  description varchar(5000) NOT NULL
 );
 
 CREATE TABLE comments (
   id serial PRIMARY KEY,
-  user varchar(50),
-  comment varchar(10000) NOT NULL,
+  username varchar(50),
+  comment varchar(5000) NOT NULL,
   article_id INT references articles(id)
 );
--- server.rb
+++ server.rb
@@ -1,18 +1,18 @@
 require 'sinatra'
 require 'sinatra/reloader'
 require 'pry'
+require 'pg'
 
-def get_articles
-  articles = []
-  CSV.foreach("articles.csv") do |row|
-    articles << {title: row[0],
-    url: row[1],
-    description: row[2]}
+def db_connection
+  begin
+    connection = PG.connect(dbname: 'slacker_news')
+
+    yield(connection)
+
+  ensure
+    connection.close
   end
-  articles
 end
-
-articles = get_articles
 
 def get_urls
   urls = []
@@ -27,51 +27,61 @@ def valid_title?(title)
   title != nil
 end
 
-def valid_url?(url)
-  urls = get_urls
-  url.include?("http://") && !urls.include?(url)
+def url_fixer(url)
+  if url[0..6] != "http://"
+    url = "http://" + url
+  end
+  url
 end
 
 def valid_description?(description)
   description.length > 20
 end
 
+def valid_url?(url)
+
+end
+
 def write_article(title, url, description)
-  CSV.open("articles.csv", "a") do |csv|
-    csv << [title, url, description]
+  article = 'INSERT INTO articles (title, url, description)
+  VALUES ($1, $2, $3)'
+
+  db_connection do |conn|
+    conn.exec_params(article, [title, url, description])
   end
 end
 
-def valid_input?(title, url, description)
-  (valid_title?(title) && valid_url?(url) && valid_description?(description))
-end
-
-def save_article(url, title, description)
-  article = { url: url, title: title, description: description }
-
+def valid_input?(title, description)
+  (valid_title?(title) && valid_description?(description))
 end
 
 get '/' do
-  articles = get_articles
-
-  erb :index, locals: {articles: articles}
+  redirect '/articles'
 end
 
-get '/submit' do
+get '/articles' do
+  query = 'SELECT articles.title, articles.url, articles.description
+  FROM articles'
 
-  erb :submit
-end
-
-post '/submit' do
-  articles = get_articles
-  if valid_input?(params["title"], params["url"], params["description"])
-        title = params["title"]
-        url = params["url"]
-        description = params["description"]
-        write_article(title, url, description)
-        redirect '/'
-    else
-      erb :submit
+  @articles = db_connection do |conn|
+      conn.exec(query)
     end
+  erb :'articles/index'
+end
 
+get '/new' do
+  erb :'new/index'
+end
+
+post '/new' do
+  articles = get_articles
+  if valid_input?(params["title"], params["description"])
+    title = params["title"]
+    url = url_fixer(params["url"])
+    description = params["description"]
+    write_article(title, url, description)
+    redirect '/articles'
+  else
+      erb :'new/index'
+  end
 end
